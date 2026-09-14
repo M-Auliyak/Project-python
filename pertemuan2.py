59cab0112800e64d1f7a301b44647588e54382ec
M-Auliyak <axioolaptopku5@gmail.com>
2026-09-14 14:12:40 +0700

    Add files via upload

---
+++ pertemuan2.py
@@ -0,0 +1,408 @@
+# function call / pemanggilan function
+def greet():
+    print('Hello World!')
+
+greet()
+
+print('Outside function')
+
+
+# argumen functtion
+def greet(name):
+    print("Hello", name)
+
+# pass argument
+greet("John")
+
+
+# function with two arguments
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print("Sum: ", sum)
+
+# function call with two values
+add_numbers(5, 4)
+
+
+# function definition
+def find_square(num):
+    result = num * num
+    return result
+
+# function call
+square = find_square(3)
+
+print('Square:', square)
+
+
+def future_function():
+    pass
+
+# this will execute without any action or error
+future_function()  
+
+
+# library
+import math
+
+# sqrt computes the square root
+square_root = math.sqrt(4)
+
+print("Square Root of 4 is",square_root)
+
+# pow() comptes the power
+power = pow(2, 3)
+
+print("2 to the power 3 is",power)
+
+
+def add_numbers(a, b):
+    sum = a + b
+    print('Sum:', sum)
+
+add_numbers(2, 3)
+
+# Output: Sum: 5
+
+
+# Function Argument with Default Values
+def add_numbers( a = 7,  b = 8):
+    sum = a + b
+    print('Sum:', sum)
+
+
+# function call with two arguments
+add_numbers(2, 3)
+
+#  function call with one argument
+add_numbers(a = 2)
+
+# function call with no arguments
+add_numbers()
+
+
+# keyword argument
+def display_info(first_name, last_name):
+    print('First Name:', first_name)
+    print('Last Name:', last_name)
+
+display_info(last_name = 'Cartman', first_name = 'Eric')
+
+
+# program to find sum of multiple numbers 
+
+def find_sum(*numbers):
+    result = 0
+    
+    for num in numbers:
+        result = result + num
+    
+    print("Sum = ", result)
+
+# function call with 3 arguments
+find_sum(1, 2, 3)
+
+# function call with 2 arguments
+find_sum(4, 9)
+
+
+# local variabel
+def greet():
+
+    # local variable
+    message = 'Hello'
+    
+    print('Local', message)
+
+greet()
+
+# try to access message variable 
+# outside greet() function
+print(message)
+
+
+# global variabel
+# declare global variable
+message = 'Hello'
+
+def greet():
+    # declare local variable
+    print('Local', message)
+
+greet()
+print('Global', message)
+
+
+# nonlocal variabel
+# outside function 
+def outer():
+    message = 'local'
+
+    # nested function  
+    def inner():
+
+        # declare nonlocal variable
+        nonlocal message
+
+        message = 'nonlocal'
+        print("inner:", message)
+
+    inner()
+    print("outer:", message)
+
+outer()
+
+
+# merubah global variabel 
+# global variable
+c = 1 
+
+def add():
+
+    # use of global keyword
+    global c
+
+    # increment c by 2
+    c = c + 2 
+
+    print(c)
+
+add()
+
+
+# rekursif function
+def factorial(x):
+    """This is a recursive function
+    to find the factorial of an integer"""
+
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
+num = 3
+print("The factorial of", num, "is", factorial(num))
+
+
+# import dan rename
+# import module by renaming it
+import math as m
+
+print(m.pi)
+
+
+# merubah directory 
+import os
+
+# change directory
+os.chdir('C:\\Python33')
+
+print(os.getcwd())
+
+
+# merubah nama directory
+import os
+
+os.listdir()
+['test']
+
+# rename a directory
+os.rename('test','new_one')
+
+os.listdir()
+['new_one']
+
+
+# menghapus directory
+import os
+
+# delete "myfile.txt" file
+os.remove("myfile.txt")
+
+import os
+
+# delete the empty directory "mydir"
+os.rmdir("mydir")
+
+
+# menulis csv file di python
+import csv
+with open('protagonist.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "Movie", "Protagonist"])
+    writer.writerow([1, "Lord of the Rings", "Frodo Baggins"])
+    writer.writerow([2, "Harry Potter", "Harry Potter"])
+
+
+# melihat csv file
+import pandas as pd
+pd.read_csv("people.csv")
+
+
+# menulis csv file menggunakan to_csv function
+import pandas as pd
+
+# creating a data frame
+df = pd.DataFrame([['Jack', 24], ['Rose', 22]], columns = ['Name', 'Age'])
+
+# writing data frame to a CSV file
+df.to_csv('person.csv')
+
+
+#  Read CSV files with csv.reader()
+import csv
+with open('innovators.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row)
+
+
+
+# Read CSV files with initial spaces
+import csv
+with open('people.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, skipinitialspace=True)
+    for row in reader:
+        print(row)
+
+
+
+# CSV files with quotes
+import csv
+with open('person1.csv', 'r') as file:
+    reader = csv.reader(file, quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    for row in reader:
+        print(row)
+
+
+
+# Read CSV files using dialect
+import csv
+csv.register_dialect('myDialect',
+                     delimiter='|',
+                     skipinitialspace=True,
+                     quoting=csv.QUOTE_ALL)
+
+with open('office.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, dialect='myDialect')
+    for row in reader:
+        print(row)
+
+
+
+# Python csv.DictReader()
+import csv
+with open("people.csv", 'r') as file:
+    csv_file = csv.DictReader(file)
+    for row in csv_file:
+        print(dict(row))
+
+
+
+# Using csv.Sniffer() to deduce the dialect of CSV files
+import csv
+with open('office.csv', 'r') as csvfile:
+    sample = csvfile.read(64)
+    has_header = csv.Sniffer().has_header(sample)
+    print(has_header)
+
+    deduced_dialect = csv.Sniffer().sniff(sample)
+
+with open('office.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, deduced_dialect)
+
+    for row in reader:
+        print(row)
+
+
+
+
+# Write into CSV files with csv.writer()
+import csv
+with open('innovators.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["SN", "Name", "Contribution"])
+    writer.writerow([1, "Linus Torvalds", "Linux Kernel"])
+    writer.writerow([2, "Tim Berners-Lee", "World Wide Web"])
+    writer.writerow([3, "Guido van Rossum", "Python Programming"])
+
+
+
+# Writing Multiple Rows with writerows()
+import csv
+row_list = [["SN", "Name", "Contribution"],
+             [1, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
+with open('protagonist.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(row_list)
+
+
+
+
+# Write CSV File Having Pipe Delimiter
+import csv
+data_list = [["SN", "Name", "Contribution"],
+             [1, "Linus Torvalds", "Linux Kernel"],
+             [2, "Tim Berners-Lee", "World Wide Web"],
+             [3, "Guido van Rossum", "Python Programming"]]
+with open('innovators.csv', 'w', newline='') as file:
+    writer = csv.writer(file, delimiter='|')
+    writer.writerows(data_list)
+
+
+# Write CSV files with quotes
+import csv
+row_list = [
+    ["SN", "Name", "Quotes"],
+    [1, "Buddha", "What we think we become"],
+    [2, "Mark Twain", "Never regret anything that made you smile"],
+    [3, "Oscar Wilde", "Be yourself everyone else is already taken"]
+]
+with open('quotes.csv', 'w', newline='') as file:
+    writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC, delimiter=';')
+    writer.writerows(row_list)
+
+
+
+#  Writing CSV files with custom quoting character
+import csv
+row_list = [
+    ["SN", "Name", "Quotes"],
+    [1, "Buddha", "What we think we become"],
+    [2, "Mark Twain", "Never regret anything that made you smile"],
+    [3, "Oscar Wilde", "Be yourself everyone else is already taken"]
+]
+with open('quotes.csv', 'w', newline='') as file:
+    writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC,
+                        delimiter=';', quotechar='*')
+    writer.writerows(row_list)
+
+
+
+#  Python csv.DictWriter()
+import csv
+
+with open('players.csv', 'w', newline='') as file:
+    fieldnames = ['player_name', 'fide_rating']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    writer.writerow({'player_name': 'Magnus Carlsen', 'fide_rating': 2870})
+    writer.writerow({'player_name': 'Fabiano Caruana', 'fide_rating': 2822})
+    writer.writerow({'player_name': 'Ding Liren', 'fide_rating': 2801})
+
+
+# Using escapechar in csv writer
+import csv
+row_list = [
+    ['Book', 'Quote'],
+    ['Lord of the Rings',
+        '"All we have to decide is what to do with the time that is given us."'],
+    ['Harry Potter', '"It matters not what someone is born, but what they grow to be."']
+]
+with open('book.csv', 'w', newline='') as file:
+    writer = csv.writer(file, escapechar='/', quoting=csv.QUOTE_NONE)
+    writer.writerows(row_list)
